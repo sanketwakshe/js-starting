5b9106dcba44226810a56f2d87ebf4a18e419c04
sanketwakshe <153482871+sanketwakshe@users.noreply.github.com>
2024-08-07 17:04:02 +0000

    compair

---
+++ basics/compair.js
@@ -0,0 +1,10 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log("2">1);
+
+
+console.log("2"==2);
+
+//stricly chaking
+console.log("3"===1); //datatype are naot matched
--- basics/conversionOperator.js
+++ basics/conversionOperator.js
@@ -1,24 +1,62 @@
  //converstions 
  
- let score = "77df"
-// console.log(score)
-console.log(typeof(score));
+//  let score = "77df"
+// // console.log(score)
+// console.log(typeof(score));
 
-let num = Number(score);
-console.log(typeof num);
-console.log(num); // ans is nan
+// let num = Number(score);
+// console.log(typeof num);
+// console.log(num); // ans is nan
 
 //"33" => 33
 //"33abc" => nan
 // true =>1 ; false =>0
-let isLogin = 0
-let booleanIsLogin = Boolean(isLogin)
-console.log(booleanIsLogin);
+// let isLogin = 0
+// let booleanIsLogin = Boolean(isLogin);
+// console.log(booleanIsLogin);
 
 // 1 => true ;0 => false;
 //"" => false
 //"sanket" => true
-let someNum = 44
-let stringNum = String(someNum)
-console.log(stringNum);
-console.log( typeof stringNum)
+// let someNum = 55
+// let stringNum = String(someNum)
+// console.log(stringNum);
+// console.log( typeof stringNum)
+
+// ****************operation****************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+ 
+// console.log(2+2);
+// console.log(2%2); reminder
+// console.log(2**3); power2
+// console.log(2/2); module
+
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+// console.log((3+4)*3%4)
+
+
+console.log(+true); // 0
+console.log(+""); // 0
+// postfix prefix mdm search
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+
+
+
+
