71d9d5bc0452e1dedd7b3f9a6b7be843e29c3d94
sanketwakshe <153482871+sanketwakshe@users.noreply.github.com>
2024-08-08 04:16:12 +0000

    datatype

---
+++ basics/datatypeSummary.js
@@ -0,0 +1,45 @@
+//primitive//only copy acces
+
+//7 types : String.Number ,Boolean,null,undefine,symbol,BigInt;
+
+// const score = 100
+// const scoreValue = 100.3
+//  const login = false
+//  const temp = null
+//  let userEmail;
+//   const id = Symbol('123')
+//   const id1 = Symbol('123') //typeObj = symbol
+//   console.log(id1===id)
+//   const BigNumber = 3432455849388949n
+
+
+// referance type(Non Primitive)
+
+//Array,Objects,Functions
+
+//[] square bracket
+
+ const hero = ["sanket","nikhil","sanika"];  //Array
+ let ObjId ={
+    name:"sanket",
+    age:22,
+ }
+
+ const MyFunction = function(){ // typeObj = function
+    console.log("sanket");
+ }
+
+
+
+
+
+
+
+
+
+ // Table of typeOf operator result
+ undefined="undefined"
+ null="object"
+ Boolean="boolean"
+ Number="number"
+ String="string"
