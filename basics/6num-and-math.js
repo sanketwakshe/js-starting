cce0ef4b8822cbfc81480990f76bee774d18d8d8
sanketwakshe <153482871+sanketwakshe@users.noreply.github.com>
2024-08-08 06:34:53 +0000

    num

---
+++ basics/6num-and-math.js
@@ -0,0 +1,18 @@
+//Number
+
+const score = 40
+// console.log(40);
+
+const balance = new Number(100.444444444)
+// console.log(balance);
+//output :[Number:40]
+ 
+// console.log(balance.toString().length)  // output:3
+// console.log(balance.toFixed(2))  //output: 100.44
+
+const onum = 123.8888
+// console.log(onum.toPrecision(3));  //output :124
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-In'));
+
