160aa3cdad6cf38f14d22e7e651c7f83c2cec4cf
sanketwakshe <153482871+sanketwakshe@users.noreply.github.com>
2024-08-08 06:17:30 +0000

    string

---
+++ basics/5string.js
@@ -0,0 +1,47 @@
+const name = "sanket"
+const age = 50
+// console.log(name + age + "value"); 
+
+ console.log(`Hellow my name is ${name.toUpperCase()} my age is ${age}`);
+
+const game = new String("cricket")
+// console.log(game.length);  //length
+
+// console.log(game.toUpperCase());  // .toUpperCase(
+
+// console.log(game.charAt(2));  //charAt 2 length which caracter
+
+// console.log(game.indexOf('t')); //indexof t
+
+const newString = game .slice(-7 ,2)
+// console.log(newString)   //slice
+
+const newString1 = "         sanket     "
+// console.log(newString1)
+const newString2 = "              sanket       "
+
+// console.log(newString2.trim());          //trim
+
+
+  //  split
+
+// const str = 'The quick brown fox jumps over the lazy dog.';
+
+// const words = str.split(' ');
+// console.log(words[3]);
+// // Expected output: "fox"              
+
+// const chars = str.split('');
+// console.log(chars[8]);
+// // Expected output: "k"
+
+// const strCopy = str.split();
+// console.log(strCopy);
+
+
+
+ const url = "http://hitesh.com/hitesh%20choudhary"
+//  console.log(url.replace('%20' , '*'));    //replace
+
+
+//  console.log(url.includes('sanket'));  //include  add
--- basics/datatypeSummary.js
+++ basics/datatypeSummary.js
@@ -1,4 +1,4 @@
-//primitive//only copy acces
+// 1) primitive//only copy acces
 
 //7 types : String.Number ,Boolean,null,undefine,symbol,BigInt;
 
@@ -11,35 +11,50 @@
 //   const id1 = Symbol('123') //typeObj = symbol
 //   console.log(id1===id)
 //   const BigNumber = 3432455849388949n
+// **************************************************************************
 
-
-// referance type(Non Primitive)
+// 2) referance type(Non Primitive)
 
 //Array,Objects,Functions
 
 //[] square bracket
 
- const hero = ["sanket","nikhil","sanika"];  //Array
- let ObjId ={
-    name:"sanket",
-    age:22,
- }
+//  const hero = ["sanket","nikhil","sanika"];  //Array
 
- const MyFunction = function(){ // typeObj = function
-    console.log("sanket");
- }
+//  let ObjId ={
+//     name:"sanket",
+//     age:22,
+//  }
+
+//  const MyFunction = function(){ // typeOf = function
+//    //  console.log("sanket");
+//  }
+// ******************************************************************
+
+// ***********************Table of typeOf operator result**********************
+//  undefined="undefined"
+//  null="object"
+//  Boolean="boolean"
+//  Number="number"
+//  String="string"
 
 
+ //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//  Memorys type
 
+// 1) Stack (Premitive types) give only copy
+// let a = "ss"
+// let b = a
+//   b="bb"
+//   console.log(a)
+//   console.log(b);
+//                   aa // copy
+//                   ss //copy
+//                   aa
+//                   ss 
+ 
 
+// *************************************************************************
+/// 2) Heap (Non Premitive type) give a value are a same
 
-
-
-
-
- // Table of typeOf operator result
- undefined="undefined"
- null="object"
- Boolean="boolean"
- Number="number"
- String="string"
+// one heap box many time request get a same value not change
