fd7f41ea8102ec81c4955f61557ea6c0507539e6
sanketwakshe <153482871+sanketwakshe@users.noreply.github.com>
2024-09-02 04:12:11 +0000

    while loop

---
+++ 5basics/two.js
@@ -0,0 +1,24 @@
+// whilw loop
+
+let index= 0 
+// while(index <=10){
+//     console.log(`value ${index}`);
+//     index = index+2;
+// }
+
+let myarry = ['sanket','sanika','jignesh']
+
+// let arr = 0
+// while (arr < myarry.length) {
+//     console.log(`value of ${myarry[arr]}`);
+//     arr = arr + 1;
+    
+// }
+
+//***do and***
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+    } while (score <=10);
