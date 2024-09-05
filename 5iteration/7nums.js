e276ab52cd047410d14599b98d569c7db055ff60
sanketwakshe <153482871+sanketwakshe@users.noreply.github.com>
2024-09-05 03:54:07 +0000

    basics

--- 5iteration/7nums.js
+++ 5iteration/7nums.js
@@ -1,14 +1,14 @@
-const myNum = [1,2,3,4,5,6,7,8,9]
+const myNum = [1,2,3,3,4,5,6,7,8,9]
 
 const newNum = myNum.map((num) =>num + 10);
-//console.log(newNum);
+console.log(newNum);
 
 const newNum1 = myNum
 .map((num)=> num*10)               // chaning value depend upon the 1 to 2
 .map((num)=> num + 1)
 .filter((num)=> num>=40)
                                    
-console.log(newNum1);
+//console.log(newNum1);
  
  
 
--- 5iteration/8reduced.js
+++ 5iteration/8reduced.js
@@ -1,14 +1,29 @@
 const myNum = [1,2,3]
 
-//  const mytotal = muNum.reduce(function(acc,currval){
+//  const mytotal = myNum.reduce(function(acc,currval){
 //     console.log(`acc ${acc} curv${currval}`);
 //     return acc + currval
 //  } ,0 )
  console.log(mytotal);
 
-const mytotal = myNum.reduce(( acc , curr)=> acc + curr ,0 )
+const mytotal = myNum.reduce(( acc , curr)=> acc + curr ,0 );
 
  //output 
 //  acc 0 curv1
 // acc 1 curv2
 // acc 3 curv3
+
+
+//syntax of reduced from mdn
+
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
